0519579efee5ae1e22e11159c34513196fce3f96
Zach Kornas <zkornas@uw.edu>
2022-02-22 20:06:24 -0800

    Working on it

---
+++ source/analysis.R
@@ -0,0 +1,4 @@
+# Load data from (https://github.com/vera-institute/incarceration-trends/blob/master/incarceration_trends.csv)
+
+prison_data <- read.csv("https://raw.githubusercontent.com/vera-institute/incarceration-trends/master/incarceration_trends.csv")
+
