a7003780ecb2b8b8c71000096b5cbec61afa86d0
Zacharia <zkornas@uw.edu>
2022-02-24 16:59:53 -0800

    Progress

--- source/analysis.R
+++ source/analysis.R
@@ -1,4 +1,31 @@
+#Load necessary packages
+install.packages("tidyverse")
+install.packages("dplyr")
+install.packages("ggplot2")
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+
 # Load data from (https://github.com/vera-institute/incarceration-trends/blob/master/incarceration_trends.csv)
 
 prison_data <- read.csv("https://raw.githubusercontent.com/vera-institute/incarceration-trends/master/incarceration_trends.csv")
+
+
+# Variable 1: Out of all the counties, which has the highest black prison population in the current year
+
+highest_black_jail_curr <- prison_data %>%
+  filter(year == max(year)) %>%
+  filter(black_jail_pop == max(black_jail_pop, na.rm = TRUE))
+  
+highest_white_jail_curr <- prison_data %>%
+  filter(year == max(year)) %>%
+  filter(white_jail_pop == max(white_jail_pop, na.rm = TRUE))
+
+highest_black_jail_curr <- prison_data %>%
+  filter(year == max(year)) %>%
+  filter(black_jail_pop == max(black_jail_pop, na.rm = TRUE))
+
+highest_black_jail_curr <- prison_data %>%
+  filter(year == max(year)) %>%
+  filter(black_jail_pop == max(black_jail_pop, na.rm = TRUE))
 
