2721b966e30bce15db65a63c52e057fd977c4fc6
zkornas <zkornas@uw.edu>
2022-02-25 21:10:06 -0800

    I'm struggling

--- source/analysis.R
+++ source/analysis.R
@@ -3,10 +3,12 @@ install.packages("tidyverse")
 install.packages("dplyr")
 install.packages("ggplot2")
 install.packages("zoo")
+install.packages("maps")
 library(tidyverse)
 library(dplyr)
 library(ggplot2)
 library(zoo)
+library(maps)
 
 # Load data from (https://github.com/vera-institute/incarceration-trends/blob/master/incarceration_trends.csv)
 
@@ -18,22 +20,52 @@ prison_data <- read.csv("https://raw.githubusercontent.com/vera-institute/incarc
 highest_black_jail_curr <- prison_data %>%
   filter(year == max(year)) %>%
   filter(black_jail_pop == max(black_jail_pop, na.rm = TRUE))
+highest_black_jail_state <- highest_black_jail_curr %>%
+  pull(state)
+highest_black_jail_county <- highest_black_jail_curr %>%
+  pull(county_name)
+highest_black_jail_pop <- highest_black_jail_curr %>%
+  pull(black_jail_pop)
   
 highest_white_jail_curr <- prison_data %>%
   filter(year == max(year)) %>%
   filter(white_jail_pop == max(white_jail_pop, na.rm = TRUE))
+highest_white_jail_state <- highest_white_jail_curr %>%
+  pull(state)
+highest_white_jail_county <- highest_white_jail_curr %>%
+  pull(county_name)
+highest_white_jail_pop <- highest_white_jail_curr %>%
+  pull(white_jail_pop)
 
 highest_latinx_jail_curr <- prison_data %>%
   filter(year == max(year)) %>%
   filter(latinx_jail_pop == max(latinx_jail_pop, na.rm = TRUE))
+highest_latinx_jail_state <- highest_latinx_jail_curr %>%
+  pull(state)
+highest_latinx_jail_county <- highest_latinx_jail_curr %>%
+  pull(county_name)
+highest_latinx_jail_pop <- highest_latinx_jail_curr %>%
+  pull(latinx_jail_pop)
 
 highest_aapi_jail_curr <- prison_data %>%
   filter(year == max(year)) %>%
   filter(aapi_jail_pop == max(aapi_jail_pop, na.rm = TRUE))
+highest_aapi_jail_state <- highest_aapi_jail_curr %>%
+  pull(state)
+highest_aapi_jail_county <- highest_aapi_jail_curr %>%
+  pull(county_name)
+highest_aapi_jail_pop <- highest_aapi_jail_curr %>%
+  pull(aapi_jail_pop)
 
 highest_other_jail_curr <- prison_data %>%
   filter(year == max(year)) %>%
   filter(other_race_jail_pop == max(other_race_jail_pop, na.rm = TRUE))
+highest_other_jail_state <- highest_other_jail_curr %>%
+  pull(state)
+highest_other_jail_county <- highest_other_jail_curr %>%
+  pull(county_name)
+highest_other_jail_pop <- highest_other_jail_curr %>%
+  pull(other_race_jail_pop)
 
 # Variable 2: Out of all the counties, which has the highest incarcerated to ratio of total black vs white population?
 
@@ -98,7 +130,7 @@ king_inc_ratio_year <- prison_data %>%
   filter(!is.na(black_jail_pop)) %>%
   filter(!is.na(white_jail_pop)) %>%
   filter(!is.na(black_pop_15to64)) %>%
-  filter(!is.na(white_pop_15to64)) %>%
+  filter(!is.na(white_pop_15to64))
 
 white_black_ratio_king_year <- ggplot(data = king_inc_ratio_year) +
   geom_line(aes(x = year, y = black_jail_pop), color = "blue") +
@@ -108,9 +140,66 @@ print(white_black_ratio_king_year)
 
 # Chart 2: Ratio of black to white population vs ratio of black to white inc population in US
 
-black_white_ratios_us <- prison_data %>%
-  filter(!is.na(black_jail_pop)) %>%
-  filter(!is.na(white_jail_pop)) %>%
-  filter(!is.na(black_pop_15to64)) %>%
-  filter(!is.na(white_pop_15to64))
+king_curr_inc_race <- inc_ratio_curr %>%
+  filter(state == "WA") %>%
+  filter(county_name == "King County") %>%
+  filter(year == max(year)) %>%
+  mutate(latinx_jail_ratio = as.numeric(latinx_jail_pop/latinx_pop_15to64)) %>%
+  mutate(aapi_jail_ratio = as.numeric(aapi_jail_pop/aapi_pop_15to64)) %>%
+  mutate(native_jail_ratio = as.numeric(native_jail_pop/native_pop_15to64))
+
+
+king_race_data <- data.frame(
+  race = c("Black", "White", "Latinx", "AAPI", "Native"),
+  inc_ratio = c(king_curr_inc_race$black_jail_ratio,
+                king_curr_inc_race$white_jail_ratio,
+                king_curr_inc_race$latinx_jail_ratio,
+                king_curr_inc_race$aapi_jail_ratio,
+                king_curr_inc_race$native_jail_ratio)
+)
+
+race_king_chart <- ggplot(king_race_data, aes(x = race, y = inc_ratio)) +
+  geom_bar(stat = "identity")
+
+print(race_king_chart)
+
+
+# Chart 3:
+
+bt <- theme_bw() +
+  theme(
+    axis.line = element_blank(),
+    axis.ticks = element_blank(),
+    axis.text = element_blank(),
+    axis.title = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.grid.major = element_blank(),
+    panel.border = element_blank(),
+    plot.background = element_blank()
+  )
+
+black_inc_percent <- inc_ratio_curr %>%
+  mutate(black_jail_percent = as.numeric(black_jail_ratio * 10))
+
+us_regions <- map_data("county") %>%
+  unite(polyname, region, subregion, sep = ",") %>%
+  left_join(county.fips, by = "polyname")
+
+map_data <- us_regions %>%
+  left_join(black_inc_percent, by = "fips")
+
+black_inc_map <- ggplot(map_data) +
+  geom_polygon(mapping = aes(x = long, y = lat, group = group, fill = black_jail_percent),
+               color = "white",
+               size = 0.0001) +
+  coord_map() +
+  scale_fill_continuous(na.value = "gray", low = "blue", high = "red") +
+  bt
+
+print(black_inc_map)
+
+#Map white ratio
+
+
+
 
